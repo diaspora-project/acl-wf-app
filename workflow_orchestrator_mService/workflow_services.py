05d8e6a46c6497e0a3b5a3f79ba3bdb0014c6409
4ua <alnajjaram@ornl.gov>
2025-10-07 13:43:31 -0400

    update Dockerfiles for all services

--- workflow_orchestrator_mService/workflow_services.py
+++ workflow_orchestrator_mService/workflow_services.py
@@ -26,7 +26,7 @@ def prepare_endpoints_for_inference(g_e_p):
     with Executor(endpoint_id=g_e_p['gid'], amqp_port=443,client=gcc) as gce:
         # ... then submit for execution, ...
         future = gce.submit(helloworld_gep,g_e_p['sys_path'])
-        print('\n################    Activate Remote Compute Worker or Globus Endpoint    ##############')
+        print('\n################    Activate Globus Endpoint    ##############')
         print(future.result())
         print(f" {g_e_p['gid']} at {g_e_p['device']}")
 
